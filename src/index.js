04554acc4d845024b401f8359f1562bbd5897bc2
Fərid <feridhelios@gmail.com>
2022-04-22 16:51:55 +0400

    local lang

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "flag-icon-css/css/flag-icons.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import App from "./App";
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi) // passes i18n down to react-i18next
+  .init({
+    supportedLngs: ["en", "ru", "az", "ar"],
+    fallbackLng: "en",
+    detection: {
+      order: ["path", "cookie", "htmlTag", "localStorage", "subdomain"],
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "/assets//locales/{{lng}}/translation.json",
+    },
+  });
+const loadingMarkup = (
+  <div className="py-4 text-center">
+    <h2>Loading...</h2>
+  </div>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>
 );
